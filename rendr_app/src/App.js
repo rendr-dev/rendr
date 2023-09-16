6c586ea23ac481ed6b68bb7b4ecad1b5d645d67d
bensonngai02 <bensonngai02@outlook.com>
2023-09-16 18:19:57 -0400

    changing font

--- rendr_app/src/App.js
+++ rendr_app/src/App.js
@@ -10,15 +10,9 @@ const App = () => {
       setInputValue(e.target.value);
     };
 
-    const handleButton = (e) => {
-
-    };
-
-
     iframe.addEventListener('load', function () {
       const iframeDocument = iframe.contentWindow.document;
 
-      
       iframeDocument.addEventListener('click', function (e) {
 
         const { clientX, clientY } = e;
@@ -27,35 +21,50 @@ const App = () => {
         const existingInput = iframeDocument.getElementById('dynamicInput');
         if (existingInput) existingInput.remove();
 
-        const existingButton = iframeDocument.getElementById('dynamicButton');
-        if (existingButton) existingButton.remove();
-
         // Create new text box
         const inputElement = iframeDocument.createElement('input');
         inputElement.type = 'text';
         inputElement.style.position = 'absolute';
         inputElement.style.left = `${clientX}px`;
-        inputElement.style.top = `${clientY}px`;
+        inputElement.style.top = `${clientY - 50}px`;
         inputElement.id = 'dynamicInput';
         inputElement.style.width = '150px'; // Adjust the width as needed
         inputElement.style.padding = '10px';
         inputElement.style.border = '2px solid #ccc';
         inputElement.style.borderRadius = '4px';
-        inputElement.placeholder = 'Type a design prompt here...';
-        inputElement.style.backgroundColor = 'orange';
+        inputElement.style.backgroundColor = '#FFC803';
         inputElement.style.color = 'black';
+        inputElement.placeholder = 'Type a design prompt here...';
 
-         // Create new button
-         const buttonElement = iframeDocument.createElement('button');
-         buttonElement.id = 'dynamicButton';
-         buttonElement.textContent = 'Enter';
-         buttonElement.style.position = 'absolute';
-         buttonElement.style.left = `${clientX + 180}px`; // Adjust the button position as needed
-         buttonElement.style.top = `${clientY}px`;
-         buttonElement.style.padding = '10px';
-         buttonElement.style.borderRadius = '4px';
-         buttonElement.style.backgroundColor = 'orange';
-         buttonElement.style.color = 'black';
+        const existingButton = iframeDocument.getElementById('dynamicButton');
+        if (existingButton) existingButton.remove();
+         // Create suggest designs button
+         const suggestDesigns = iframeDocument.createElement('button');
+         suggestDesigns.id = 'dynamicButton';
+         suggestDesigns.textContent = 'Suggest Designs';
+         suggestDesigns.style.position = 'absolute';
+         suggestDesigns.style.left = `${clientX + 180}px`; // Adjust the button position as needed
+         suggestDesigns.style.top = `${clientY - 50}px`;
+         suggestDesigns.style.border = '2px solid #ccc';
+         suggestDesigns.style.padding = '10px';
+         suggestDesigns.style.borderRadius = '5px';
+         suggestDesigns.style.backgroundColor = '#C9FF55';
+         suggestDesigns.style.color = 'black';
+
+         const analyzeButton = iframeDocument.getElementById('dynamicButton');
+         if (analyzeButton) analyzeButton.remove();
+         // Create analyze design button
+         const analyzeDesigns = iframeDocument.createElement('button');
+         analyzeDesigns.id = 'dynamicButton';
+         analyzeDesigns.textContent = 'Analyze UI/UX';
+         analyzeDesigns.style.position = 'absolute';
+         analyzeDesigns.style.left = `${clientX + 300}px`; // Adjust the button position as needed
+         analyzeDesigns.style.top = `${clientY - 50 }px`;
+         analyzeDesigns.style.border = '2px solid #ccc';
+         analyzeDesigns.style.padding = '10px';
+         analyzeDesigns.style.borderRadius = '5px';
+         analyzeDesigns.style.backgroundColor = '#FF99EF';
+         analyzeDesigns.style.color = 'black';
 
         // Attach event to handle input
         inputElement.addEventListener('input', handleInput);
@@ -65,18 +74,19 @@ const App = () => {
         // Attach event to handle Enter key press
         inputElement.addEventListener('keydown', (event) => {
           if (event.key === 'Enter') {
-            if (inputElement && buttonElement && inputElement.value.trim() === '') {
+            if (inputElement && suggestDesigns && inputElement.value.trim() === '') {
               // If input is empty on Enter, remove the input element
               inputElement.remove();
-              buttonElement.remove();
+              suggestDesigns.remove();
+              analyzeDesigns.remove()
             }
           }
         });
 
-        
-    
         iframeDocument.body.appendChild(inputElement);
-        iframeDocument.body.appendChild(buttonElement);
+        iframeDocument.body.appendChild(suggestDesigns);
+        iframeDocument.body.appendChild(analyzeDesigns);
+
         inputElement.focus();
 
       });
