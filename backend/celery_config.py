f11ab595e3a31401b9ad2a87a121d80f5f64a5e6
john7rho <johnkrho@gmail.com>
2023-09-21 17:56:36 -0400

    working heroku deployment
    super sex

--- backend/celery_config.py
+++ backend/celery_config.py
@@ -1,29 +1,29 @@
-from redis import SSLConnection
+# from redis import SSLConnection
 
 # Deprecated settings for local development
 # BROKER_URL = 'redis://localhost:6379/0' 
 # CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 # Redis URL for Heroku
-BROKER_URL = 'rediss://:p5f37b645dce7a876b4564a7e85c63324dbeb7ffbe7a901f91da4220695387f46@ec2-54-145-71-12.compute-1.amazonaws.com:10969'
-CELERY_RESULT_BACKEND = 'rediss://:p5f37b645dce7a876b4564a7e85c63324dbeb7ffbe7a901f91da4220695387f46@ec2-54-145-71-12.compute-1.amazonaws.com:10969'
+BROKER_URL = 'redis://:p5f37b645dce7a876b4564a7e85c63324dbeb7ffbe7a901f91da4220695387f46@ec2-54-160-230-30.compute-1.amazonaws.com:29359'
+CELERY_RESULT_BACKEND = 'redis://:p5f37b645dce7a876b4564a7e85c63324dbeb7ffbe7a901f91da4220695387f46@ec2-54-160-230-30.compute-1.amazonaws.com:29359'
 
 # SSL support for the Redis broker
-BROKER_USE_SSL = {
-    'ssl_cert_reqs': SSLConnection.CERT_REQUIRED,
-    'ssl_keyfile': None,
-    'ssl_certfile': None,
-    'ssl_ca_certs': None,
-    'ssl_check_hostname': False,
-}
+# BROKER_USE_SSL = {
+#     'ssl_cert_reqs': SSLConnection.CERT_NONE,
+#     'ssl_keyfile': None,
+#     'ssl_certfile': None,
+#     'ssl_ca_certs': None,
+#     'ssl_check_hostname': False,
+# }
 
 # Transport options (can be adjusted based on your needs)
-BROKER_TRANSPORT_OPTIONS = {
-    'visibility_timeout': 3600,
-    'queue_order_strategy': 'sorted',
-    'socket_keepalive': True,
-    'retry_on_timeout': True
-}
+# BROKER_TRANSPORT_OPTIONS = {
+#     'visibility_timeout': 3600,
+#     'queue_order_strategy': 'sorted',
+#     'socket_keepalive': True,
+#     'retry_on_timeout': True
+# }
 
 # Note: If you're using the latest version of Celery, some configurations 
 # like BROKER_URL might need to be updated to use the new naming conventions
